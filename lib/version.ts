81ec36b15f484f36547143389fda423a058793e2
duarte-victor-dev <268436219+duarte-victor-dev@users.noreply.github.com>
2026-04-10 19:09:42 -0300

    release: v2.3.0

--- lib/version.ts
+++ lib/version.ts
@@ -1,6 +1,6 @@
 // Auto-generated - do not edit manually
 // Updated via: npm run version:patch
 
-export const APP_VERSION = '2.2.9'
-export const BUILD_DATE = '2026-04-10T21:57:55.276Z'
-export const BUILD_NUMBER = 1775858275277
+export const APP_VERSION = '2.3.0'
+export const BUILD_DATE = '2026-04-10T22:08:03.207Z'
+export const BUILD_NUMBER = 1775858883208
