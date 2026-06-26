105bd51d4c85113f7a737e21bbad2bee85953c92
Duarte Victor <xcgyb7dcsd@privaterelay.appleid.com>
2026-06-25 21:33:49 -0300

    style(cookies): eye icon emerald green matching button
    Changed icon color from dark blue (#0f1729) to brand-primary emerald green.
    Matches 'Aceitar todos' button for visual consistency and improved visibility.

--- src/components/ui/cookie-consent.tsx
+++ src/components/ui/cookie-consent.tsx
@@ -260,7 +260,7 @@ export function CookieConsentBanner() {
                   }}
                 >
                   {/* Eye Icon with Glow */}
-                  <svg className="icon-glow-animation h-6 w-6 text-[#0f1729]" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="icon-glow-animation h-6 w-6 text-brand-primary" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
                   </svg>
                 </div>
