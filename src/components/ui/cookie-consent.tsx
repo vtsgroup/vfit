bf79500a369d776fd03177ef25d78bcff41ac7e7
Duarte Victor <xcgyb7dcsd@privaterelay.appleid.com>
2026-06-25 21:36:25 -0300

    style(cookies): dark outline eye with green fill
    Eye SVG:
    - Fill: green (#22c55e)
    - Stroke: dark blue (#0f1729, 1.5px width)
    Creates contrast: green background with dark outline

--- src/components/ui/cookie-consent.tsx
+++ src/components/ui/cookie-consent.tsx
@@ -259,9 +259,9 @@ export function CookieConsentBanner() {
                     boxShadow: 'inset 0 1px 1px rgba(255,255,255,0.1)'
                   }}
                 >
-                  {/* Eye Icon with Glow */}
+                  {/* Eye Icon with Dark Outline + Green Background */}
                   <svg className="icon-glow-animation h-6 w-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path fill="#22c55e" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
+                    <path fill="#22c55e" stroke="#0f1729" strokeWidth="1.5" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
                   </svg>
                 </div>
                 <div>
