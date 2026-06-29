804af0c10bef1f6468322c151957357c935731df
Duarte Victor <xcgyb7dcsd@privaterelay.appleid.com>
2026-06-28 21:14:19 -0300

    chore: build metadata churn (v5.3.0 fast deploy)

--- lib/version.ts
+++ lib/version.ts
@@ -2,5 +2,5 @@
 // Updated via: npm run version:patch
 
 export const APP_VERSION = '5.3.0'
-export const BUILD_DATE = '2026-06-28T23:59:39.624Z'
-export const BUILD_NUMBER = 1782691179625
+export const BUILD_DATE = '2026-06-29T00:04:21.129Z'
+export const BUILD_NUMBER = 1782691461130
