65aaed8cb5e067af215348b15d592c1e50afaa24
Duarte Victor <xcgyb7dcsd@privaterelay.appleid.com>
2026-07-09 09:14:44 -0300

    release: v5.5.6

--- lib/version.ts
+++ lib/version.ts
@@ -1,6 +1,6 @@
 // Auto-generated - do not edit manually
 // Updated via: npm run version:patch
 
-export const APP_VERSION = '5.5.5'
-export const BUILD_DATE = '2026-07-09T12:10:32.761Z'
-export const BUILD_NUMBER = 1783599032762
+export const APP_VERSION = '5.5.6'
+export const BUILD_DATE = '2026-07-09T12:12:26.645Z'
+export const BUILD_NUMBER = 1783599146668
